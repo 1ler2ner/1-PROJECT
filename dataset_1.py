b9c96644f27ca714f087387a5bb8d6d3da232bef
ler2ner <ler2ner@yandex.com>
2026-02-15 13:33:18 +0200

    before Extract BALTI-STRASENI time series

--- dataset_1.py
+++ dataset_1.py
@@ -1,8 +1,15 @@
 
 import pandas as pd
 
-data = pd.read_excel(r"D:\ALEX\ICS\U_1min_2025.xlsx", sheet_name="10.25",decimal=",")
-print(data.columns)
+data = pd.read_excel(
+    r"D:\ALEX\ICS\U_1min_2025.xlsx",
+    sheet_name="10.25",
+    skiprows=4,
+    decimal=","
+)
+
+pd.set_option("display.max_columns", None)
+print(data.head())
 print(data.shape)
 
-print(data.head(10))
+
