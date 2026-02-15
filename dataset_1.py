8268516e0a286cde38d54e825c35c17a37be14fe
ler2ner <ler2ner@yandex.com>
2026-02-15 14:30:39 +0200

    Add exceedance analysis for BALTI-STRASENI

--- dataset_1.py
+++ dataset_1.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 
 data = pd.read_excel(
@@ -8,8 +7,28 @@ data = pd.read_excel(
     decimal=","
 )
 
-pd.set_option("display.max_columns", None)
-print(data.head())
-print(data.shape)
+balti_str = pd.DataFrame({
+    "timestamp": data["Unnamed: 2"],
+    "BALTI-STRASENI": data["Unnamed: 8"]
+})
 
+balti_str = balti_str.set_index("timestamp")
 
+pd.set_option("display.max_rows", None)
+print(balti_str.head(10))
+
+max_time = balti_str["BALTI-STRASENI"].idxmax()
+max_value = balti_str["BALTI-STRASENI"].max()
+
+print("Max value:", max_value)
+print("At time:", max_time)
+
+print(balti_str["BALTI-STRASENI"].describe())
+
+Unom = 330
+threshold = 1.1 * Unom
+
+exceed = balti_str[balti_str["BALTI-STRASENI"] > threshold]
+
+print(exceed.head())
+print("Number of exceedances:", len(exceed))
